c14f90c1c130f205ad07507f58ab134ce899aa35
이종남 <jongnammac@MacBookAir.local>
2025-02-19 15:22:15 +0900

    header/footer/main commit

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <!-- <Header />
+  <Section />
+  <Footer />
+  <router-view /> -->
+</template>
+<script>
+import Header from "./Header.vue";
+import Section from "./Section.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "Main",
+  components: { Header, Section, Footer },
+};
+</script>
+<style>
+
+</style>
